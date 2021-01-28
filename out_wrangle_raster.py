5f09c54cb2f2b34bda2fde9ddacd3c6a38019807
mallen <05michaelallen@gmail.com>
2021-01-28 11:21:12 -0500

    new plots

--- out_wrangle_raster.py
+++ out_wrangle_raster.py
@@ -39,19 +39,23 @@ lfn =  glob.glob("../data/rf_output/*.tif")
 
 # import - try other standardizations
 complst = []
+runmeta = []
 for f in lfn:
     fi = rio.open(f)
     complst.append(fi.read()[0])
-    
+    # import metadata
+    runmeta.append(np.array(pd.read_csv(f[:38] + "vabparams.txt").iloc[:,1]))
 complst = np.array(complst)
+runmeta = pd.DataFrame(runmeta).T
+runmeta.columns = lfn
+print(runmeta)
 
 # =============================================================================
 # analysis
 # =============================================================================
 # plot eg
-fig, ax0 = plt.subplots(1, 1)
-p0 = ax0.imshow(complst[2], vmin = -2, vmax = 2)
-
+#fig, ax0 = plt.subplots(1, 1)
+#p0 = ax0.imshow(complst[2], vmin = -2, vmax = 2)
 
 # plot difference? west - east
 complst_d = complst[0] - complst[1]
@@ -63,21 +67,37 @@ plt.colorbar(p0)
 # reshape
 lcrs = pd.DataFrame(lcr.reshape([lcr.shape[0], lcr.shape[1]*lcr.shape[2]]).T, columns = classes)
 lamcrs = pd.DataFrame(lamcr.reshape([lamcr.shape[1]*lamcr.shape[2]]).T, columns = ['lamcr'])
-complsts = pd.DataFrame(complst_d.reshape([complst_d.shape[0]*complst_d.shape[1]]).T, columns = ['lfn'])
+complsts = pd.DataFrame(complst.reshape([complst.shape[0], complst.shape[1]*complst.shape[2]]).T, columns = [lfn])
 # concat
-merge = pd.concat([lcrs, lamcrs, complsts], axis = 1)
+merge = pd.concat([complsts, lcrs, lamcrs], axis = 1)
 
 # filter for bulding fraction, nas
 merge = merge.dropna()
-mergeb = merge[merge['building'] > 0.5]
-plt.scatter(mergeb['lamcr'], mergeb['lst_diff'], alpha = 0.05)
+mergeb = merge[merge['building'] > -1]
+#plt.scatter(mergeb['lamcr'], mergeb['lst_diff'], alpha = 0.05)
 
-# bin based on lc, dsm??
+### bin based on lc, dsm??
+# label based on nadir, west view, east view (check lfn, these get a random from glob)
+nadir = 1
+west = 0
+east = 2
+
+# bin
 bins = [1, 2, 4, 6, 8, 15, 50]
-binned = []
 bin_centers = []
+n = []
+w = []
+e = []
 for b in range(6):
-    binned.append(np.mean(mergeb[(mergeb['lamcr'] > bins[b]) & (mergeb['lamcr'] < bins[b+1])]['lst_diff']))
+    n.append(np.mean(mergeb[(mergeb['lamcr'] > bins[b]) & (mergeb['lamcr'] < bins[b+1])])[nadir])
+    w.append(np.mean(mergeb[(mergeb['lamcr'] > bins[b]) & (mergeb['lamcr'] < bins[b+1])])[west])
+    e.append(np.mean(mergeb[(mergeb['lamcr'] > bins[b]) & (mergeb['lamcr'] < bins[b+1])])[east])
     bin_centers.append((bins[b+1]+bins[b])/2)
 
-plt.scatter(bin_centers, binned)
+# plot a scatter
+fig, ax0 = plt.subplots(1, 1)
+ax0.plot(bin_centers, n, color = 'black', label = 'Nadir')
+ax0.plot(bin_centers, w, color = 'indianred', label = 'Sensor West')
+ax0.plot(bin_centers, e, color = 'dodgerblue', label = 'Sensor East')
+
+### plot as boxplots????
